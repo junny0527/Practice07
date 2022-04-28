e888d2f104df1e342d5d6a0e9009837534aa234b
junny0527 <bonjun03@naver.com>
2022-04-28 16:47:36 +0900

    2022 04 28

---
+++ src/com/javaex/ex02/Bird.java
@@ -0,0 +1,30 @@
+package com.javaex.ex02;
+
+public abstract class Bird {
+	protected String name;
+
+	public Bird() {
+	}
+
+	public Bird(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Bird [name=" + name + "]";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+
+	}
+
+	public abstract void fly();
+	public abstract void sing();
+	public abstract void showName();
+}
